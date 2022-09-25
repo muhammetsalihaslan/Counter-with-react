1a292daf7b4114add53903e984b83f9017c181b6
salih <muhammetsalihaslan@gmail.com>
2022-09-25 22:41:54 +0300

    3

--- counter/src/components/counter/Counter.jsx
+++ counter/src/components/counter/Counter.jsx
@@ -3,13 +3,25 @@ import {useState} from 'react';
 
 const Counter = () => {
     const [counter, setCounter] = useState(0)
+
+    const increase = () => {
+        setCounter(count => count + 1);
+    }
+
+    const decrease = () => {
+        setCounter(count => count - 1);
+    }
+
+    const reset = () => {
+        setCounter(0)
+    }
     return( 
         <div>
          <div className="counter">
 
-         <button className="btn">-</button>
-         <button className="btn-r">reset</button>
-         <button className="btn">+</button>
+         <button className="btn" onClick={decrease}>-</button>
+         <button className="btn-r"onClick={reset}>reset</button>
+         <button className="btn" onClick={increase}>+</button>
          </div>
          <div className="zero">{counter}</div>
         </div>
